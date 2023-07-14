72a1e62368d5ee9ce2a3f1f3026d86b3425a1b27
hovik-gevorgyan <122265643+hovik-gevorgyan@users.noreply.github.com>
2023-07-14 17:54:44 +0400

    Add files via upload

---
+++ CPractice3.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+
+void printArray(int numbers[], int index);
+void checkFifthBit(int number);
+int isYearEndOfCentury(int year);
+void isYearALeapYear(int year);
+int gcd(int a, int b);
+void findGcdOfThreeNumbers(int a, int b, int c);
+
+
+
+int main() {
+    printf("Hello World \n");
+   
+    findGcdOfThreeNumbers(12,15,21);
+    isYearALeapYear(2024);
+    checkFifthBit(44);
+
+    return 0;
+}
+
+
+void printArray(int numbers[], int index) {
+   for(int j = index - 1; j >= 0; j--) {
+     printf("%d", numbers[j]);
+   }
+}
+
+void checkFifthBit(int number) {
+    int bitNumber[32];
+    int index = 0;
+
+    while(number > 0) {
+        bitNumber[index] = number % 2;
+        number /= 2;
+        index++;
+    }
+    printf("Initial number in binary is \n");
+    printArray(bitNumber, index);
+    printf("\n");
+
+    if(index >= 4) {
+        if(bitNumber[4] == 0) {
+            bitNumber[4] = 1;
+            printf("The binary number after modification is \n");
+            printArray(bitNumber, index);
+            printf("\n");
+        } else {
+            printf("%d \n", number);
+        }
+    } else {
+        printf("The number has no 5th bit \n");
+    }
+}
+
+
+int isYearEndOfCentury(int year) {
+    if(year % 100 == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
+void isYearALeapYear(int year) {
+    if(isYearEndOfCentury(year)) {
+        if(year % 400 == 0) {
+            printf("The year is a leap year \n");
+        } else {
+            printf("The year is not a leap year \n");
+        }
+    } else {
+        if(year % 4 == 0) {
+            printf("The year is a leap year \n");
+        } else {
+            printf("The year is not a leap year \n");
+        }
+    }
+}
+
+
+int gcd(int a, int b) {
+    if(b % a == 0) {
+        return a;
+    } else {
+        return gcd(b % a, a);
+    }
+}
+
+
+void findGcdOfThreeNumbers(int a, int b, int c) {
+    int divisor = gcd(a, gcd(b, c));
+    printf("GCD is %d \n", divisor);
+}
+
+
